e63d77b647de67c98b77a0beab62e1360d24898d
kaibalya sahoo <120CR0406@nitrkl.ac.in>
2021-03-12 08:51:56 +0530

    Initial Commit

---
+++ calculator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  calculator
+//
+//  Created by KAIBALYA SAHOO on 21/12/1942 Saka.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
